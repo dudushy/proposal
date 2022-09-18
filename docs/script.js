5ff195def9272eb970dc0e61fbbc320f03e622f0
dudushy <eduardotalarico1337@gmail.com>
2022-09-18 16:25:06 -0300

    First working version

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,33 @@
-console.log("[template-web] Hello World!");
+console.log("[proposal] init");
+
+var height = window.innerHeight;
+var width = window.innerWidth;
+
+console.log("[proposal] height", height);
+console.log("[proposal] width", width);
+
+window.onresize = function () {
+  height = window.innerHeight;
+  width = window.innerWidth;
+
+  console.log("[proposal#onresize] height", height);
+  console.log("[proposal#onresize] width", width);
+}
+
+const ButtonYes = document.getElementById("proposal-yes");
+ButtonYes.addEventListener("click", () => {
+  location.href = "yes/index.html"
+})
+
+const ButtonNo = document.getElementById("proposal-no");
+ButtonNo.addEventListener("click", () => {
+  console.log("[proposal#click] height", height);
+  console.log("[proposal#click] width", width);
+
+  const randomHeight = Math.floor(Math.random() * (height - 61) + 0);
+  const randomWidth = Math.floor(Math.random() * (width - 80) + 0);;
+
+  ButtonNo.style.position = "absolute";
+  ButtonNo.style.top = `${randomHeight}px`; //? height
+  ButtonNo.style.left = `${randomWidth}px`; //? width
+})
